9d86935df116723543d127d699f259717d9aefa1
Shanna J. Smith <shanna_smith987@hotmail.com>
2021-12-15 22:45:25 -0500

    import and format more nav icons

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { FaPhoneAlt } from 'react-icons/fa';
+import { IconContext } from 'react-icons'
+import { FaPhoneAlt } from 'react-icons/fa'
+import { BsFillPersonFill } from 'react-icons/bs'
+import { BiDialpadAlt } from 'react-icons/bi'
+import { MdSettings } from 'react-icons/md'
 
 const Navigation = () => {
   return (
     <footer>
-      <p><FaPhoneAlt /> Navigation</p>
+      <span className="nav-icons">
+        <IconContext.Provider value={{ color: 'green', size: 30 }}>
+          <FaPhoneAlt /> <BsFillPersonFill /> <BiDialpadAlt />
+          <MdSettings />
+        </IconContext.Provider>
+      </span>
     </footer>
   )
 }
