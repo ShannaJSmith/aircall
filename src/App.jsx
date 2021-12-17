45b6e9c64b38dec8cfceedd2655ef39ae4d8f5b5
Shanna J. Smith <shanna_smith987@hotmail.com>
2021-12-17 01:41:39 -0500

    install axios and implement get request for calldata

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Header from './Header.jsx'
 import Navigation from './components/Navigation.jsx'
@@ -8,79 +9,101 @@ import ActivityFeed from './components/ActivityFeed.jsx'
 
 const App = () => {
   const [callData, setCallData] = useState([
-    {
-      id: 7834,
-      created_at: '2018-04-19T09:38:41.000Z',
-      direction: 'outbound',
-      from: 'Pierre-Baptiste Béchu',
-      to: '06 46 62 12 33',
-      via: 'NYC Office',
-      duration: '120',
-      is_archived: false,
-      call_type: 'missed',
-    },
-    {
-      id: 7833,
-      created_at: '2018-04-18T16:59:48.000Z',
-      direction: 'outbound',
-      from: 'Jonathan Anguelov',
-      to: '06 45 13 53 91',
-      via: 'NYC Office',
-      duration: '60',
-      is_archived: false,
-      call_type: 'missed',
-    },
-    {
-      id: 7832,
-      created_at: '2018-04-18T16:53:22.000Z',
-      direction: 'inbound',
-      from: '06 19 18 23 92',
-      to: 'Jonathan Anguelov',
-      via: 'Support FR',
-      duration: '180',
-      is_archived: false,
-      call_type: 'answered',
-    },
-    {
-      id: 7831,
-      created_at: '2018-04-18T16:42:55.000Z',
-      direction: 'inbound',
-      from: '06 34 45 74 34',
-      to: 'Xavier Durand',
-      via: 'Support FR',
-      duration: '180',
-      is_archived: false,
-      call_type: 'answered',
-    },
-    {
-      id: 7830,
-      created_at: '2018-04-18T16:23:43.000Z',
-      direction: 'inbound',
-      from: '+33 6 34 45 74 34',
-      to: null,
-      via: 'Support FR',
-      duration: '120',
-      is_archived: false,
-      call_type: 'voicemail',
-    },
-    {
-      id: 7829,
-      created_at: '2018-04-18T15:43:32.000Z',
-      direction: 'inbound',
-      from: '+33 6 34 45 74 34',
-      to: 'Olivier Pailhes',
-      via: 'Spain Hotline',
-      duration: '300',
-      is_archived: false,
-      call_type: 'answered',
-    },
+    //   {
+    //     id: 7834,
+    //     created_at: '2018-04-19T09:38:41.000Z',
+    //     direction: 'outbound',
+    //     from: 'Pierre-Baptiste Béchu',
+    //     to: '06 46 62 12 33',
+    //     via: 'NYC Office',
+    //     duration: '120',
+    //     is_archived: false,
+    //     call_type: 'missed',
+    //   },
+    //   {
+    //     id: 7833,
+    //     created_at: '2018-04-18T16:59:48.000Z',
+    //     direction: 'outbound',
+    //     from: 'Jonathan Anguelov',
+    //     to: '06 45 13 53 91',
+    //     via: 'NYC Office',
+    //     duration: '60',
+    //     is_archived: false,
+    //     call_type: 'missed',
+    //   },
+    //   {
+    //     id: 7832,
+    //     created_at: '2018-04-18T16:53:22.000Z',
+    //     direction: 'inbound',
+    //     from: '06 19 18 23 92',
+    //     to: 'Jonathan Anguelov',
+    //     via: 'Support FR',
+    //     duration: '180',
+    //     is_archived: false,
+    //     call_type: 'answered',
+    //   },
+    //   {
+    //     id: 7831,
+    //     created_at: '2018-04-18T16:42:55.000Z',
+    //     direction: 'inbound',
+    //     from: '06 34 45 74 34',
+    //     to: 'Xavier Durand',
+    //     via: 'Support FR',
+    //     duration: '180',
+    //     is_archived: false,
+    //     call_type: 'answered',
+    //   },
+    //   {
+    //     id: 7830,
+    //     created_at: '2018-04-18T16:23:43.000Z',
+    //     direction: 'inbound',
+    //     from: '+33 6 34 45 74 34',
+    //     to: null,
+    //     via: 'Support FR',
+    //     duration: '120',
+    //     is_archived: false,
+    //     call_type: 'voicemail',
+    //   },
+    //   {
+    //     id: 7829,
+    //     created_at: '2018-04-18T15:43:32.000Z',
+    //     direction: 'inbound',
+    //     from: '+33 6 34 45 74 34',
+    //     to: 'Olivier Pailhes',
+    //     via: 'Spain Hotline',
+    //     duration: '300',
+    //     is_archived: false,
+    //     call_type: 'answered',
+    //   },
   ])
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const serverData = await fetchData();
+  //     setCallData(serverData);
+  //   };
+  //   getData();
+
+  // }, []);
+
+  useEffect(() => {
+    axios
+      .get('https://aircall-job.herokuapp.com/activities')
+      .then((res) => setCallData(res.data))
+  }, [])
+
+  // const fetchData = async () => {
+  //   const res = await fetch('https://aircall-job.herokuapp.com/activities');
+  //   const data = await res.json();
+  //   console.log('data:', data);
+  //   return data;
+  // };
 
   return (
     <div className="container">
       <Header />
       <div className="container-view">Some activities should be here</div>
-      <ActivityFeed callData={callData}/>
+      <ActivityFeed callData={callData} />
       <Navigation />
     </div>
   )
