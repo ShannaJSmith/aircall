976ef049bf8a7a97b2b547c9a4c3476e9c3c00c6
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-01-26 23:07:07 -0500

    callList.css additions

--- src/components/CallList.jsx
+++ src/components/CallList.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import {
   BsFillTelephoneOutboundFill,
   BsFillTelephoneInboundFill,
 } from 'react-icons/bs'
 
-const CallList = ({ data }) => {
+const CallList = ({ data}) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+
   return (
-    <div>
-      {data.direction === 'outbound' ? (
-        <BsFillTelephoneOutboundFill />
-      ) : (
-        <BsFillTelephoneInboundFill />
-      )}
-      <div className="calls">
-        <p
-          style={
-            data.call_type === 'missed'
-              ? { color: 'red' }
-              : data.call_type === 'answered'
-              ? { color: 'green' }
-              : null
-          }
-        >
-          {data.call_type}
-        </p>
-        <p>{data.from} called</p>
-      </div>
+    <div className="calls">
+      <p
+        style={
+          data.call_type === 'missed'
+            ? { color: 'red' }
+            : data.call_type === 'answered'
+            ? { color: 'green' }
+            : null
+        }
+      >
+        {data.direction === 'outbound' ? (
+          <BsFillTelephoneOutboundFill />
+        ) : (
+          <BsFillTelephoneInboundFill />
+        )}
+        {data.call_type}
+      </p>
+      <p>{data.from} called</p>
       <p>at {data.created_at.slice(11, 16)}</p>
     </div>
   )
